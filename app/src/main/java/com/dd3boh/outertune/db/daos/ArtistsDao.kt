Muzi-Powered-by-KannanTech/Muzi

commit e50570aefda0231177d54ead7abe1353b5de22e8

add n/M download count to albums/artists/playlists
- add n/M download count to albums/artists/playlists
- move entity and maps queries to separate Daos

app/src/main/java/com/dd3boh/outertune/db/daos/ArtistsDao.kt
@@ -15,6 +15,7 @@ import com.dd3boh.outertune.constants.ArtistSortType
 import com.dd3boh.outertune.db.entities.Artist
 import com.dd3boh.outertune.db.entities.ArtistEntity
 import com.dd3boh.outertune.db.entities.Song
+import com.dd3boh.outertune.db.entities.SongArtistMap
 import com.dd3boh.outertune.extensions.reversed
 import com.dd3boh.outertune.ui.utils.resize
 import com.zionhuang.innertube.pages.ArtistPage
@@ -22,62 +23,96 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import java.time.LocalDateTime
 
+/*
+ * Logic related to artists entities and their mapping
+ */
+
 @Dao
 interface ArtistsDao {
 
     // region Gets
-    @Query("SELECT *, (SELECT COUNT(1) FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE artistId = artist.id AND song.inLibrary IS NOT NULL) AS songCount FROM artist WHERE id = :id")
+    @Query("""
+        SELECT 
+            artist.*,
+            COUNT(song.id) AS songCount,
+            SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
+        FROM artist
+            LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
+            LEFT JOIN song ON sam.songId = song.id
+        WHERE artist.id = :id AND song.inLibrary IS NOT NULL
+        GROUP BY artist.id
+    """)
     fun artist(id: String): Flow<Artist?>
 
     @Query("SELECT * FROM artist WHERE name = :name")
     fun artistByName(name: String): ArtistEntity?
 
-    @Transaction
+    @Query("""
+        SELECT 
+            artist.*,
+            COUNT(song.id) AS songCount,
+            SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
+        FROM artist
+            LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
+            LEFT JOIN song ON sam.songId = song.id
+        WHERE artist.name LIKE '%' || :query || '%' AND song.inLibrary IS NOT NULL
+        GROUP BY artist.id
+        HAVING songCount > 0
+        LIMIT :previewSize
+    """)
+    fun searchArtists(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Artist>>
+
+    @Query("SELECT song.* FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE song_artist_map.artistId IN (SELECT id FROM artist WHERE name LIKE '%' || :query || '%') LIMIT :previewSize")
+    fun searchArtistSongs(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Song>>
+
+    @Query("SELECT * FROM artist WHERE name LIKE '%' || :query || '%' LIMIT :previewSize")
+    fun fuzzySearchArtists(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<ArtistEntity>>
+
     @Query("SELECT * FROM artist WHERE isLocal != 1")
     fun allRemoteArtists(): Flow<List<ArtistEntity>>
 
-    @Transaction
     @Query("SELECT * FROM artist WHERE isLocal = 1")
     fun allLocalArtists(): Flow<List<ArtistEntity>>
 
-    @Transaction
     @Query("SELECT song.* FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE artistId = :artistId AND inLibrary IS NOT NULL LIMIT :previewSize")
     fun artistSongsPreview(artistId: String, previewSize: Int = 3): Flow<List<Song>>
 
-    @Transaction
     @Query("""
-        SELECT artist.*,
-               (SELECT COUNT(1)
-                FROM song_artist_map
-                         JOIN song ON song_artist_map.songId = song.id
-                WHERE artistId = artist.id
-                  AND song.inLibrary IS NOT NULL) AS songCount
+        SELECT 
+            artist.*,
+            COUNT(song.id) AS songCount,
+            SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
         FROM artist
-                 JOIN(SELECT artistId, SUM(songTotalPlayTime) AS totalPlayTime
-                      FROM song_artist_map
-                               JOIN (SELECT songId, SUM(playTime) AS songTotalPlayTime
-                                     FROM event
-                                     WHERE timestamp > :fromTimeStamp
-                                     GROUP BY songId) AS e
-                                    ON song_artist_map.songId = e.songId
-                      GROUP BY artistId
-                      ORDER BY totalPlayTime DESC
-                      LIMIT :limit)
-                     ON artist.id = artistId
+            LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
+            LEFT JOIN song ON sam.songId = song.id
+            LEFT JOIN (
+                SELECT 
+                    songId, 
+                    SUM(playTime) AS songTotalPlayTime
+                FROM event
+                WHERE timestamp > :fromTimeStamp
+                GROUP BY songId
+            ) AS e ON sam.songId = e.songId
+        WHERE song.inLibrary IS NOT NULL
+        GROUP BY artist.id
+        ORDER BY SUM(e.songTotalPlayTime) DESC
+        LIMIT :limit
     """)
     fun mostPlayedArtists(fromTimeStamp: Long, limit: Int = 6): Flow<List<Artist>>
 
-    @Transaction
     @RawQuery(observedEntities = [ArtistEntity::class])
     fun _getArtists(query: SupportSQLiteQuery): Flow<List<Artist>>
 
     // region Artists Sort
     private fun queryArtists(orderBy: String): SimpleSQLiteQuery {
         return SimpleSQLiteQuery("""
-            SELECT artist.*, COUNT(song.id) AS songCount
+            SELECT 
+                artist.*,
+                COUNT(song.id) AS songCount,
+                SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
             FROM artist
-                INNER JOIN song_artist_map ON artist.id = song_artist_map.artistId
-                INNER JOIN song ON song_artist_map.songId = song.id
+                LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
+                LEFT JOIN song ON sam.songId = song.id
             WHERE song.inLibrary IS NOT NULL
             GROUP BY artist.id
             HAVING songCount > 0
@@ -106,10 +141,13 @@ interface ArtistsDao {
     // region Bookmarked Artists Sort
     private fun queryArtistsBookmarked(orderBy: String): SimpleSQLiteQuery {
         return SimpleSQLiteQuery("""
-            SELECT artist.*, COUNT(song.id) AS songCount
+            SELECT 
+                artist.*, 
+                COUNT(song.id) AS songCount,
+                SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
             FROM artist
-                INNER JOIN song_artist_map ON artist.id = song_artist_map.artistId
-                INNER JOIN song ON song_artist_map.songId = song.id
+                LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
+                LEFT JOIN song ON sam.songId = song.id
             WHERE artist.bookmarkedAt IS NOT NULL
             GROUP BY artist.id
             HAVING songCount > 0
@@ -138,13 +176,16 @@ interface ArtistsDao {
     // region Downloaded Artists Sort
     private fun queryArtistsWithDonwloads(orderBy: String): SimpleSQLiteQuery {
         return SimpleSQLiteQuery("""
-            SELECT artist.*, COUNT(song.id) AS songCount
+            SELECT 
+                artist.*, 
+                COUNT(song.id) AS songCount,
+                SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
             FROM artist
-                INNER JOIN song_artist_map ON artist.id = song_artist_map.artistId
-                INNER JOIN song ON song_artist_map.songId = song.id
-            WHERE song.dateDownload IS NOT NULL
+                LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
+                LEFT JOIN song ON sam.songId = song.id
+            WHERE song.inLibrary IS NOT NULL
             GROUP BY artist.id
-            HAVING songCount > 0
+            HAVING downloadCount > 0
             ORDER BY $orderBy
         """)
     }
@@ -168,15 +209,12 @@ interface ArtistsDao {
     // endregion
 
     // region Artist Songs Sort
-    @Transaction
     @Query("SELECT song.* FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE artistId = :artistId AND inLibrary IS NOT NULL ORDER BY inLibrary")
     fun artistSongsByCreateDateAsc(artistId: String): Flow<List<Song>>
 
-    @Transaction
     @Query("SELECT song.* FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE artistId = :artistId AND inLibrary IS NOT NULL ORDER BY title COLLATE NOCASE ASC")
     fun artistSongsByNameAsc(artistId: String): Flow<List<Song>>
 
-    @Transaction
     @Query("SELECT song.* FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE artistId = :artistId AND inLibrary IS NOT NULL ORDER BY totalPlayTime")
     fun artistSongsByPlayTimeAsc(artistId: String): Flow<List<Song>>
 
@@ -192,6 +230,9 @@ interface ArtistsDao {
     // region Inserts
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(artist: ArtistEntity)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(map: SongArtistMap)
     // endregion
 
     // region Updates
@@ -208,6 +249,10 @@ interface ArtistsDao {
             )
         )
     }
+
+    @Transaction
+    @Query("UPDATE song_artist_map SET artistId = :newId WHERE artistId = :oldId")
+    fun updateSongArtistMap(oldId: String, newId: String)
     // endregion
 
     // region Deletes

app/src/main/java/com/dd3boh/outertune/db/DatabaseDao.kt
@@ -7,7 +7,6 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.RawQuery
 import androidx.room.Transaction
-import androidx.room.Update
 import androidx.room.Upsert
 import androidx.sqlite.db.SupportSQLiteQuery
 import com.dd3boh.outertune.db.daos.AlbumsDao
@@ -15,18 +14,14 @@ import com.dd3boh.outertune.db.daos.ArtistsDao
 import com.dd3boh.outertune.db.daos.PlaylistsDao
 import com.dd3boh.outertune.db.daos.QueueDao
 import com.dd3boh.outertune.db.daos.SongsDao
-import com.dd3boh.outertune.db.entities.Album
 import com.dd3boh.outertune.db.entities.AlbumArtistMap
 import com.dd3boh.outertune.db.entities.AlbumEntity
-import com.dd3boh.outertune.db.entities.Artist
 import com.dd3boh.outertune.db.entities.ArtistEntity
 import com.dd3boh.outertune.db.entities.Event
 import com.dd3boh.outertune.db.entities.EventWithSong
 import com.dd3boh.outertune.db.entities.FormatEntity
 import com.dd3boh.outertune.db.entities.GenreEntity
 import com.dd3boh.outertune.db.entities.LyricsEntity
-import com.dd3boh.outertune.db.entities.Playlist
-import com.dd3boh.outertune.db.entities.PlaylistSongMap
 import com.dd3boh.outertune.db.entities.QueueEntity
 import com.dd3boh.outertune.db.entities.QueueSongMap
 import com.dd3boh.outertune.db.entities.RelatedSongMap
@@ -40,7 +35,6 @@ import com.dd3boh.outertune.extensions.toSQLiteQuery
 import com.dd3boh.outertune.models.MediaMetadata
 import com.dd3boh.outertune.models.MultiQueueObject
 import com.dd3boh.outertune.models.toMediaMetadata
-import com.zionhuang.innertube.models.AlbumItem
 import com.zionhuang.innertube.models.SongItem
 import com.zionhuang.innertube.pages.AlbumPage
 import kotlinx.coroutines.flow.Flow
@@ -48,7 +42,6 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao {
 
-    @Transaction
     @Query("""
         SELECT song.*
         FROM (SELECT *, COUNT(1) AS referredCount
@@ -85,51 +78,6 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
     @Query("SELECT * FROM lyrics WHERE id = :id")
     fun lyrics(id: String?): Flow<LyricsEntity?>
 
-    @Transaction
-    fun addSongToPlaylist(playlist: Playlist, songIds: List<String>) {
-        var position = playlist.songCount
-        songIds.forEach { id ->
-            insert(
-                PlaylistSongMap(
-                    songId = id,
-                    playlistId = playlist.id,
-                    position = position++
-                )
-            )
-        }
-    }
-
-    // region Search
-    @Transaction
-    @Query("SELECT * FROM song WHERE title LIKE '%' || :query || '%' AND inLibrary IS NOT NULL LIMIT :previewSize")
-    fun searchSongs(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Song>>
-
-    @Transaction
-    @Query("SELECT * FROM song WHERE title LIKE '%' || :query || '%' AND isLocal = 1 LIMIT :previewSize")
-    fun searchSongsAllLocal(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Song>>
-
-    @Transaction
-    @Query("SELECT *, (SELECT COUNT(1) FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE artistId = artist.id AND song.inLibrary IS NOT NULL) AS songCount FROM artist WHERE name LIKE '%' || :query || '%' AND songCount > 0 LIMIT :previewSize")
-    fun searchArtists(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Artist>>
-
-    @Transaction
-    @Query("SELECT song.* FROM song_artist_map JOIN song ON song_artist_map.songId = song.id WHERE song_artist_map.artistId IN (SELECT id FROM artist WHERE name LIKE '%' || :query || '%') LIMIT :previewSize")
-    fun searchArtistSongs(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Song>>
-
-    @Transaction
-    @Query("SELECT * FROM artist WHERE name LIKE '%' || :query || '%' LIMIT :previewSize")
-    fun fuzzySearchArtists(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<ArtistEntity>>
-
-    @Transaction
-    @Query("SELECT * FROM album WHERE title LIKE '%' || :query || '%' AND EXISTS(SELECT * FROM song WHERE song.albumId = album.id AND song.inLibrary IS NOT NULL) LIMIT :previewSize")
-    fun searchAlbums(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Album>>
-
-    @Transaction
-    @Query("SELECT *, (SELECT COUNT(*) FROM playlist_song_map WHERE playlistId = playlist.id) AS songCount FROM playlist WHERE name LIKE '%' || :query || '%' LIMIT :previewSize")
-    fun searchPlaylists(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<Playlist>>
-    // endregion
-
-    @Transaction
     @Query("SELECT * FROM event ORDER BY rowId DESC")
     fun events(): Flow<List<EventWithSong>>
 
@@ -148,7 +96,6 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
     @Query("SELECT * FROM genre WHERE title = :name")
     fun genreByName(name: String): GenreEntity?
 
-    @Transaction
     @Query("SELECT * FROM genre WHERE title LIKE '%' || :query || '%' LIMIT :previewSize")
     fun genreByAproxName(query: String, previewSize: Int = Int.MAX_VALUE): Flow<List<GenreEntity>>
 
@@ -156,19 +103,7 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
     fun insert(genre: GenreEntity)
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(map: SongArtistMap)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(map: SongAlbumMap)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(map: AlbumArtistMap)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(map: SongGenreMap)
-
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insert(map: PlaylistSongMap)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(searchHistory: SearchHistory)
@@ -259,40 +194,6 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
     }
 
     @Transaction
-    fun insert(albumItem: AlbumItem) {
-        if (insert(AlbumEntity(
-                id = albumItem.browseId,
-                playlistId = albumItem.playlistId,
-                title = albumItem.title,
-                year = albumItem.year,
-                thumbnailUrl = albumItem.thumbnail,
-                songCount = 0,
-                duration = 0
-            )) == -1L
-        ) return
-        albumItem.artists
-            ?.map { artist ->
-                ArtistEntity(
-                    id = artist.id ?: artistByName(artist.name)?.id ?: ArtistEntity.generateArtistId(),
-                    name = artist.name
-                )
-            }
-            ?.onEach(::insert)
-            ?.mapIndexed { index, artist ->
-                AlbumArtistMap(
-                    albumId = albumItem.browseId,
-                    artistId = artist.id,
-                    order = index
-                )
-            }
-            ?.forEach(::insert)
-    }
-
-    @Update
-    fun update(map: PlaylistSongMap)
-
-
-    @Transaction
     fun update(album: AlbumEntity, albumPage: AlbumPage) {
         update(
             album.copy(
@@ -333,25 +234,11 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
             ?.forEach(::insert)
     }
 
-    @Transaction
-    @Query("UPDATE song_artist_map SET artistId = :newId WHERE artistId = :oldId")
-    fun updateSongArtistMap(oldId: String, newId: String)
-
-    @Transaction
-    @Query("UPDATE album_artist_map SET artistId = :newId WHERE artistId = :oldId")
-    fun updateAlbumArtistMap(oldId: String, newId: String)
-
-    @Upsert
-    fun upsert(map: SongAlbumMap)
-
     @Upsert
     fun upsert(lyrics: LyricsEntity)
 
     @Upsert
     fun upsert(format: FormatEntity)
-
-    @Delete
-    fun delete(playlistSongMap: PlaylistSongMap)
 
     @Delete
     fun delete(lyrics: LyricsEntity)
@@ -361,10 +248,6 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
 
     @Delete
     fun delete(event: Event)
-
-    @Transaction
-    @Query("DELETE FROM song_artist_map WHERE songId = :songID")
-    fun unlinkSongArtists(songID: String)
 
     @Transaction
     @Query("DELETE FROM genre WHERE isLocal = 1")
@@ -377,12 +260,6 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
         nukeLocalAlbums()
         nukeLocalGenre()
     }
-
-    @Query("SELECT * FROM playlist_song_map WHERE songId = :songId")
-    fun playlistSongMaps(songId: String): List<PlaylistSongMap>
-
-    @Query("SELECT * FROM playlist_song_map WHERE playlistId = :playlistId AND position >= :from ORDER BY position")
-    fun playlistSongMaps(playlistId: String, from: Int): List<PlaylistSongMap>
 
     @RawQuery
     fun raw(supportSQLiteQuery: SupportSQLiteQuery): Int

app/src/main/java/com/dd3boh/outertune/db/MusicDatabase.kt
@@ -106,6 +106,7 @@ class MusicDatabase(
         AutoMigration(from = 11, to = 12, spec = Migration11To12::class),
         AutoMigration(from = 12, to = 13, spec = Migration12To13::class), // Migration from InnerTune
         AutoMigration(from = 13, to = 14), // Initial queue as database
+        AutoMigration(from = 15, to = 16), // Add dateDownload to songs
     ]
 )
 @TypeConverters(Converters::class)

commit 2ab99544b0e5506bbc427c71fe2370a9a268d9f1

app: db: Fix artist DAO method
 * This made it impossible to retrieve artists and due to that
   broke the artist liking feature
 * Bug introduced in e50570aefda0231177d54ead7abe1353b5de22e8
 * Fixes second part of #328

app/src/main/java/com/dd3boh/outertune/db/daos/ArtistsDao.kt
@@ -39,8 +39,8 @@ interface ArtistsDao {
             SUM(CASE WHEN song.dateDownload IS NOT NULL THEN 1 ELSE 0 END) AS downloadCount
         FROM artist
             LEFT JOIN song_artist_map sam ON artist.id = sam.artistId
-            LEFT JOIN song ON sam.songId = song.id
-        WHERE artist.id = :id AND song.inLibrary IS NOT NULL
+            LEFT JOIN song ON sam.songId = song.id AND song.inLibrary IS NOT NULL
+        WHERE artist.id = :id
         GROUP BY artist.id
     """)
     fun artist(id: String): Flow<Artist?>